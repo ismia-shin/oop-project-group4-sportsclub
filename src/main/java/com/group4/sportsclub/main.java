28fdee05bc2c004b5a4c262ff867adf43305c9e0
ismia-shin <islamsamiha700@gmail.com>
2025-11-16 14:12:06 +0600

    Added project Structure. Individual directories for each member and subdirectories for each user. /data file added for .dat and .text files. Updated module-info.

--- src/main/java/com/group4/sportsclub/main.java
+++ src/main/java/com/group4/sportsclub/main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource(""));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Brother's Union");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,16 @@ module com.group4.sportsclub {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens com.group4.sportsclub.Samiha.FinancialOfficer to javafx.fxml;
+    opens com.group4.sportsclub.Samiha.Physician to javafx.fxml;
+
+    opens com.group4.sportsclub.Abid.MembershipHolder to javafx.fxml;
+    opens com.group4.sportsclub.Abid.OperationsManager to javafx.fxml;
+
+    opens com.group4.sportsclub.Suzane.FacilitiesManager to javafx.fxml;
+    opens com.group4.sportsclub.Suzane.MarketingManager to javafx.fxml;
+
+    opens com.group4.sportsclub.Common to javafx.fxml;
 
     opens com.group4.sportsclub to javafx.fxml;
     exports com.group4.sportsclub;
