a917eff75590c5013be66b3c46897f330f1a835c
ismia-shin <islamsamiha700@gmail.com>
2025-11-17 14:58:16 +0600

    Bugs fixed in module-info.java opens to javafx.base

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,14 @@ module com.group4.sportsclub {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.base;
 
-    opens com.group4.sportsclub.Samiha.FinancialOfficer to javafx.fxml;
-    opens com.group4.sportsclub.Samiha.Physician to javafx.fxml;
-    opens com.group4.sportsclub.Samiha.Member to javafx.fxml;
-    opens com.group4.sportsclub.Samiha.Other to javafx.fxml;
+    opens com.group4.sportsclub.Samiha.FinancialOfficer to javafx.fxml, javafx.base;
+    opens com.group4.sportsclub.Samiha.Physician to javafx.fxml, javafx.base;
+    opens com.group4.sportsclub.Samiha.Member to javafx.fxml, javafx.base;
+    opens com.group4.sportsclub.Samiha.Other to javafx.fxml, javafx.base;
+
+
 
     opens com.group4.sportsclub.Abid.MembershipHolder to javafx.fxml;
     opens com.group4.sportsclub.Abid.OperationsManager to javafx.fxml;
@@ -14,7 +17,7 @@ module com.group4.sportsclub {
     opens com.group4.sportsclub.Suzane.FacilitiesManager to javafx.fxml;
     opens com.group4.sportsclub.Suzane.MarketingManager to javafx.fxml;
 
-    opens com.group4.sportsclub.Common to javafx.fxml;
+    opens com.group4.sportsclub.Common to javafx.fxml, javafx.base;
 
     opens com.group4.sportsclub to javafx.fxml;
     exports com.group4.sportsclub;
