b495ea5a87022af51c2af453bc1e1767b076ac93
ismia-shin <islamsamiha700@gmail.com>
2025-12-09 04:41:55 +0600

    Financial Officer data bugs fixed

--- src/main/java/com/group4/sportsclub/Common/DataLoaderController.java
+++ src/main/java/com/group4/sportsclub/Common/DataLoaderController.java
@@ -1,5 +1,6 @@
 package com.group4.sportsclub.Common;
 
+import com.group4.sportsclub.Samiha.FinancialOfficer.FinancialOfficer;
 import com.group4.sportsclub.Samiha.FinancialOfficer.Refund;
 import com.group4.sportsclub.Samiha.Physician.ClearanceRequest;
 import javafx.event.ActionEvent;
@@ -23,16 +24,23 @@ public class DataLoaderController
     @javafx.fxml.FXML
     public void initialize() {
     }
-    ArrayList<Refund> refunds = new ArrayList<>();
+    ArrayList<FinancialOfficer> refunds = new ArrayList<>();
 
     @javafx.fxml.FXML
     public void loadData(ActionEvent actionEvent){
+
+        FinancialOfficer f1 = new FinancialOfficer("Hasnat Abdullah",301,"HA1");
+        FinancialOfficer f2 = new FinancialOfficer("Khalid Hossain",302,"KH2");
+
+        refunds.add(f1);
+        refunds.add(f2);
+
 
     }
 
     @javafx.fxml.FXML
     public void saveData(ActionEvent actionEvent) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data/Samiha/FinancialOfficer/refunds.bin"))) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data/Samiha/User/financialOfficerList.dat"))) {
 
             oos.writeObject(new ArrayList<>(refunds));
 
